9702d8afb222ba65f27ff87683888450d82c33ba
AleksandrKamen <Kamen.alexandr@mail.ru>
2024-05-01 09:56:37 +0300

    add: task-tracker-api, dependencies

---
+++ task-tracker-api/src/main/java/org/galaxy/tasktrackerapi/TaskTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package org.galaxy.tasktrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerApiApplication.class, args);
+    }
+
+}
