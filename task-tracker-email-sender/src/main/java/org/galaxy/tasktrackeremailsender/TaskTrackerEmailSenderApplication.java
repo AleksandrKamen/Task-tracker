e5120bc04b7c4d0ac86d0f6d33e14794cd9db6e5
AleksandrKamen <Kamen.alexandr@mail.ru>
2024-05-06 20:03:03 +0300

    add: new module - task-tracker-email-sender

---
+++ task-tracker-email-sender/src/main/java/org/galaxy/tasktrackeremailsender/TaskTrackerEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package org.galaxy.tasktrackeremailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerEmailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerEmailSenderApplication.class, args);
+    }
+
+}
---
+++ task-tracker-email-sender/src/test/java/org/galaxy/tasktrackeremailsender/TaskTrackerEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.galaxy.tasktrackeremailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerEmailSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
