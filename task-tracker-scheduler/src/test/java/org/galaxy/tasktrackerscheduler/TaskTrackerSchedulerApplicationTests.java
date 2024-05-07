5ee0e5c9b00fb73b603bf2db6f261e6050361564
AleksandrKamen <Kamen.alexandr@mail.ru>
2024-05-07 11:41:57 +0300

    add: module task-tracker-scheduler

---
+++ task-tracker-scheduler/src/test/java/org/galaxy/tasktrackerscheduler/TaskTrackerSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.galaxy.tasktrackerscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
