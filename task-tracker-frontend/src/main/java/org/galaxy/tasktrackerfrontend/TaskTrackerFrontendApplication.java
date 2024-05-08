ef13ef7ff4273eff49121f71e809bb97f548779c
AleksandrKamen <Kamen.alexandr@mail.ru>
2024-05-08 15:25:37 +0300

    add: task-tracker-frontend module

---
+++ task-tracker-frontend/src/main/java/org/galaxy/tasktrackerfrontend/TaskTrackerFrontendApplication.java
@@ -0,0 +1,13 @@
+package org.galaxy.tasktrackerfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerFrontendApplication.class, args);
+    }
+
+}
---
+++ task-tracker-frontend/src/test/java/org/galaxy/tasktrackerfrontend/TaskTrackerFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.galaxy.tasktrackerfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
