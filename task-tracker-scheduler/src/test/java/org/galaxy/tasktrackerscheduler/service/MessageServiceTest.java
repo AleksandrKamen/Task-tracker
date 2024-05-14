d04a0317597125e921be543815b8a49df9d592f9
AleksandrKamen <Kamen.alexandr@mail.ru>
2024-05-14 17:54:37 +0300

    add: docker-compose.yml

--- task-tracker-scheduler/src/test/java/org/galaxy/tasktrackerscheduler/service/MessageServiceTest.java
+++ task-tracker-scheduler/src/test/java/org/galaxy/tasktrackerscheduler/service/MessageServiceTest.java
@@ -6,19 +6,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Testcontainers
 @SpringBootTest
 @ActiveProfiles("dev")
 class MessageServiceTest {
     @Autowired
     MessageService messageService;
     User user;
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres");
 
     @BeforeEach
     void init() {
